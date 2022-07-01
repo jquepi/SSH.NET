4636c1b1aa489c495b0849c1563700b094a91dda
Nelson Susanto <nelson.hai.97@gmail.com>
2022-07-01 10:12:49 +1000

    Sensible defaults (#6)
    * Update Nuke and dependencies
    
    * Add Test target
    
    * Add TeamCity adapter
    
    * Some fixes and update sdk to net6
    
    * Revert sdk

--- nuke/Build.cs
+++ nuke/Build.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Linq;
+using JetBrains.Annotations;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
-using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.OctoVersion;
 using Nuke.Common.Utilities.Collections;
-using Nuke.OctoVersion;
-using OctoVersion.Core;
-using OctoVersion.Core.OutputFormatting.TeamCity;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
@@ -27,38 +20,31 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-        public static int Main () => Execute<Build>(x => x.Pack);
-
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Solution] readonly Solution Solution;
-    [NukeOctoVersion] readonly OctoVersionInfo OctoVersionInfo;
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
 
-    OctoVersionInfo NewVersion;
+    [Parameter("Branch name for OctoVersion to use to calculate the version number. Can be set via the environment variable OCTOVERSION_CurrentBranch.",
+        Name = "OCTOVERSION_CurrentBranch")]
+    readonly string BranchName;
+
+    [Parameter("Whether to auto-detect the branch name - this is okay for a local build, but should not be used under CI.")]
+    readonly bool AutoDetectBranch = IsLocalBuild;
+
+    [OctoVersion(UpdateBuildNumber = true, BranchParameter = nameof(BranchName),
+        AutoDetectBranchParameter = nameof(AutoDetectBranch), Framework = "net6.0")]
+    readonly OctoVersionInfo OctoVersionInfo;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TempDir => RootDirectory / "build" / "temp";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     AbsolutePath LocalPackagesDirectory => RootDirectory / ".." / "LocalPackages";
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj", "**/TestResults").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(TempDir);
-            EnsureCleanDirectory(ArtifactsDirectory);
-        });
 
-    Target CalculateVersion => _ => _
-        .DependsOn(Clean)
+    Target Clean => _ => _
         .Executes(() =>
         {
-            // generate new version and overwrite teamcity build number
-            var octoVersion = new OctoVersionInfo(2020, 0, 1, $"{OctoVersionInfo.PreReleaseTag}{OctoVersionInfo.Patch}", OctoVersionInfo.BuildMetadata);
-            var teamCityOutputFormatter = new TeamCityOutputFormatter();
-            teamCityOutputFormatter.Write(octoVersion);
-            NewVersion = octoVersion;
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
         });
 
     Target Restore => _ => _
@@ -68,34 +54,44 @@ class Build : NukeBuild
             DotNetRestore(s => s
                 .SetProjectFile(Solution));
         });
+
     Target Compile => _ => _
-        .DependsOn(CalculateVersion)
         .DependsOn(Restore)
-        .DependsOn(Clean)
         .Executes(() =>
         {
             DotNetBuild(s => s
                 .SetProjectFile(Solution)
                 .SetConfiguration(Configuration)
-                .SetVersion(NewVersion.FullSemVer)
-                .SetInformationalVersion(NewVersion.BuildMetadataWithPlus)
+                .SetVersion(OctoVersionInfo.FullSemVer)
+                .SetInformationalVersion(OctoVersionInfo.InformationalVersion)
                 .EnableNoRestore()
             );
         });
 
+    [PublicAPI]
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore());
+        });
+
     Target Pack => _ => _
-        .DependsOn(CalculateVersion)
         .DependsOn(Compile)
         .Executes(() =>
         {
             DotNetPack(s => s
                 .SetProject(Solution)
+                .SetVersion(OctoVersionInfo.FullSemVer)
                 .SetConfiguration(Configuration)
                 .SetOutputDirectory(ArtifactsDirectory)
-                .SetRunCodeAnalysis(false)
-                .SetIncludeSymbols(false)
-                .SetNoBuild(false) // Don't change this flag we need it because of https://github.com/dotnet/msbuild/issues/5566
-                .AddProperty("Version", NewVersion.FullSemVer )
+                .DisableRunCodeAnalysis()
+                .DisableIncludeSymbols()
+                .DisableNoBuild() // Don't change this flag we need it because of https://github.com/dotnet/msbuild/issues/5566
             );
         });
 
@@ -105,7 +101,13 @@ class Build : NukeBuild
         .Executes(() =>
         {
             EnsureExistingDirectory(LocalPackagesDirectory);
-            GlobFiles(ArtifactsDirectory, $"*.{NewVersion.FullSemVer}.nupkg").ForEach(x => CopyFileToDirectory(x, LocalPackagesDirectory, FileExistsPolicy.Overwrite));
+            ArtifactsDirectory.GlobFiles("*.nupkg")
+                .ForEach(package => CopyFileToDirectory(package, LocalPackagesDirectory, FileExistsPolicy.Overwrite));
         });
 
+    Target Default => _ => _
+        .DependsOn(Pack)
+        .DependsOn(CopyToLocalPackages);
+    
+    public static int Main () => Execute<Build>(x => x.Default);
 }
